4a853d0a555219749f8c64758e5358fcaeb77d10
kursatkarapinar <kursatkarapinar@gmail.com>
2025-06-16 21:25:02 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -32,7 +32,7 @@ secim_oncelikli = st.selectbox(
     oncelikli_yatirim_secim_listesi
 )
 
-col1, col2 = st.columns([2, 1])
+col1, col2 = st.columns([3, 2])
 
 with col2:
     # 1) BİRLEŞİK YAZDIRARAK SATIR BOŞLUĞUNU KALDIRMA
