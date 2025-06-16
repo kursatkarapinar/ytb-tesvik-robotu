2b18b2c791919947280e45917d5b9ea23e38d929
kursatkarapinar <kursatkarapinar@gmail.com>
2025-06-16 21:28:21 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -45,7 +45,7 @@ col1, col2 = st.columns([3, 2])
 with col2:
     # 1) BİRLEŞİK YAZDIRARAK SATIR BOŞLUĞUNU KALDIRMA
     # st.write tek çağrıda “  \n” (iki boşluk + newline) ile satır sonu verir
-    st.write("**Kürşat Karapınar - Apsis Danışmanlık**  \n")
+    st.write("**Kürşat Karapınar**  \n")
 
 # 5) Sorgula
 
