c0b11e3bdc43cf9091f9ac90631eb4a1b4ba9bbf
kursatkarapinar <kursatkarapinar@gmail.com>
2025-07-30 00:13:36 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -17,7 +17,7 @@ with col2:
 
 # 0) NACE Kodunu Seçiniz
 
-st.subtitle("NACE Kodunu Seçiniz (Kelime Bazlı Arama)")
+st.subheader("NACE Kodunu Seçiniz (Kelime Bazlı Arama)")
 
 # 0) Tüm kod–tanım çiftlerini hazırla
 faaliyet_options = [
