582ae491b156ea687ea03407cf505c323e1fc706
kursatkarapinar <kursatkarapinar@gmail.com>
2025-08-19 22:19:07 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -252,7 +252,7 @@ if st.button("Sorgula"):
 
 import streamlit as st
 
-col1, col2, col3 = st.columns([1, 5, 1])
+col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     st.title("KOSGEB")
 st.caption("Kapasite Geliştirme - Küresel Rekabetçilik için büyüme rakamları hesaplama\n")
